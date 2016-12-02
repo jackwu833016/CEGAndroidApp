f9e6c32e25ea3f14db5f522efe805c12d9248f86
steveo556 <sau059@uottawa.ca>
2016-12-01 20:37:24 -0500

    HERE WE GOOO

--- app/src/main/java/ca/uottawa/segapp/Profile.java
+++ app/src/main/java/ca/uottawa/segapp/Profile.java
@@ -1,6 +1,8 @@
 package ca.uottawa.segapp;
 
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.content.Intent;
 import android.support.v7.app.AlertDialog;
@@ -15,5 +17,59 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+    }
+
+    //goTo methods
+    public void goToHomepage (View view)
+    {
+        if (view.getId() == R.id.Back)
+        {
+            startHomepage();
+        }
+    }
+
+    public void goToProfileEdit (View view)
+    {
+        if (view.getId() == R.id.button2) {
+            startProfileEdit();
+
+        }
+    }
+
+    //start methods
+    public void startProfileEdit()
+    {
+        Intent i = new Intent(Profile.this, ProfileEdit.class);
+        startActivity(i);
+    }
+
+    public void startHomepage()
+    {
+        Intent i = new Intent(Profile.this, HomePage.class);
+        startActivity(i);
+    }
+
+    //getting the user input
+    public static String getName(Context context)
+    {
+        SharedPreferences prefs = context.getSharedPreferences("CEGAndroidApp", 0);
+        return prefs.getString("editText3", "");
+    }
+
+    public static String getUsername(Context context)
+    {
+        SharedPreferences prefs = context.getSharedPreferences("CEGAndroidApp", 0);
+        return prefs.getString("editText4", "");
+    }
+
+    public static String getAge(Context context)
+    {
+        SharedPreferences prefs = context.getSharedPreferences("CEGAndroidApp", 0);
+        return prefs.getString("editText5", "");
+    }
+
+    public static String getImage(Context context)
+    {
+        SharedPreferences prefs = context.getSharedPreferences("CEGAndroidApp", 0);
     }
 }
--- app/src/main/java/ca/uottawa/segapp/ProfileEdit.java
+++ app/src/main/java/ca/uottawa/segapp/ProfileEdit.java
@@ -1,14 +1,58 @@
 package ca.uottawa.segapp;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class ProfileEdit extends AppCompatActivity {
 
-
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
     }
 
+    //goTo Methods
+    public void goToProfile (View view)
+    {
+        if (view.getId() == R.id.profileButton) {
+            startProfile();
+
+        }
+    }
+
+    //start Methods
+    public void startProfile ()
+    {
+        Intent i = new Intent(ProfileEdit.this, Profile.class);
+        startActivity(i);
+    }
+
+    //setting the user input
+    public static void setName(Context context, String name)
+    {
+        SharedPreferences prefs = context.getSharedPreferences("CEGAndroidApp", 0);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("editText3", name);
+        editor.commit();
+    }
+
+    public static void setUsername(Context context, String username)
+    {
+        SharedPreferences prefs = context.getSharedPreferences("CEGAndroidApp", 0);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("editText4", username);
+        editor.commit();
+    }
+
+    public static void setAge(Context context, String age)
+    {
+        SharedPreferences prefs = context.getSharedPreferences("CEGAndroidApp", 0);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("editText5", age);
+        editor.commit();
+    }
 }
